1b82bd77a9509686ff0e0796b2d97106577514f4
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-01-09 10:12:30 +0100

    update: modules with usefixtures

---
+++ test/unit/fixtures/test_setenv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -=-<[ Bismillahirrahmanirrahim ]>-=-
+# -*- coding: utf-8 -*-
+# @Date    : 2023-01-09 10:07:55
+# @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+# @Link    : link
+# @Version : 1.0.0
+
+
+import os
+import tempfile
+
+import pytest
+
+
+@pytest.fixture
+def cleandir():
+    with tempfile.TemporaryDirectory() as newpath:
+        old_cwd = os.getcwd()
+        os.chdir(newpath)
+        yield
+        os.chdir(old_cwd)
+
+
+
+@pytest.mark.usefixtures("cleandir")
+class TestDirectoryInit:
+    def test_cwd_starts_empty(self):
+        assert os.listdir(os.getcwd()) == []
+        with open("myfile", "w") as f:
+            f.write("hello")
+    
+
+    def test_cwd_again_starts_empty(self):
+        assert os.listdir(os.getcwd()) == []
