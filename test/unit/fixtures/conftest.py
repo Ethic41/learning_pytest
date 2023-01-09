02fcbf31dcfb0b2de1103c07515847b3a602b160
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-01-09 09:17:44 +0100

    update: fixtures support introspection

--- test/unit/fixtures/conftest.py
+++ test/unit/fixtures/conftest.py
@@ -12,6 +12,12 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def smtp_connection():
-    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
+def smtp_connection(request):
+    server = getattr(request.module, "smtpserver", "smtp.gmail.com")
+    smtp_connection = smtplib.SMTP(server, 587, timeout=5)
+    yield smtp_connection
+    print(f"finalizing {smtp_connection} ({server})")
+    smtp_connection.close()
+
+
 
