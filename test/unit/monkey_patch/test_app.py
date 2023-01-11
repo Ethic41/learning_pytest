684d86cd7c7a39ab4361659beddfd05191e10217
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-01-11 08:12:33 +0100

    update: mocking using fixtures

--- test/unit/monkey_patch/test_app.py
+++ test/unit/monkey_patch/test_app.py
@@ -7,6 +7,7 @@
 # @Version : 1.0.0
 
 
+import pytest
 import requests
 
 import app
@@ -19,12 +20,16 @@ class MockResponse:
         return {"mock_key": "mock_response"}
 
 
-def test_get_json(monkeypatch):
+@pytest.fixture
+def mock_response(monkeypatch):
 
     def mock_get(*args, **kwargs):
         return MockResponse()
     
     monkeypatch.setattr(requests, "get", mock_get)
+
+
+def test_get_json(mock_response):
 
     result = app.get_json("https://fakeurl")
     assert result["mock_key"] == "mock_response"
